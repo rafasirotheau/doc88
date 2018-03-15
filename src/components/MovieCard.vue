e5927521ef507adada026819068119956d21c062
rafasirotheau <rsirotheau@gmail.com>
2018-03-15 20:33:38 -0300

    added MovieList & MovieCard components

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="movie-card">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieCard',
+}
+</script>
+
+<style src="./MovieCard.scss" scoped lang="scss"></style>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="movie-list">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieList',
+}
+</script>
+
+<style src="./MovieList.scss" scoped lang="scss"></style>
